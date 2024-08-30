c7af1395ea50125e9b0ddc9e794a18ba68de4a4d
Rafael <rafael.mota8@gmail.com>
2024-08-30 19:17:18 +0100

    Tela inicial do App. Utilizando edittext e recuperando o valor dos edittext

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.comunidadedevspace.imc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Recuperar os componentes EditText
+        // Criar uma variavel e associar o componente de UI<EditText>
+        // Recuperar o botao da tela
+        // Recuperar o texto digitado no edt peso
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener{
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+
+            println("Seu peso é: " + peso)
+            println("Sua altura é: " + altura)
+        }
     }
 }
